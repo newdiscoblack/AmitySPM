dcad35cc567c9721b5e1263a6a7e35b375738837
Kacper Jagiełło <kacperjagieo@gmail.com>
2024-07-10 13:11:57 +0200

    Second commit

--- Package.swift
+++ Package.swift
@@ -6,20 +6,44 @@ import PackageDescription
 let package = Package(
     name: "AmitySPM",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "AmitySPM",
-            targets: ["AmitySPM"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AmitySPM"
-        ),
-        .testTarget(
-            name: "AmitySPMTests",
-            dependencies: ["AmitySPM"]
-        ),
-    ]
+         .library(
+             name: "AmityUIKit",
+             targets: [
+                "AmityUIKit",
+                "AmitySDK",
+                "Realm",
+                "RealmSwift",
+                "AmityUIKitLiveStream",
+                "UpstraUIKit"
+             ]
+         )
+     ],
+     targets: [
+         .binaryTarget(
+             name: "AmityUIKit",
+             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.20.7/AmityUIKit.xcframework.zip",
+             checksum: "081f8a47d910e50a712375f4b9dc659ec8e4feec9d1d142857fee3e51e1e0105"
+         ),
+         .binaryTarget(
+             name: "AmitySDK",
+             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.20.7/AmitySDK.xcframework.zip",
+             checksum: "64fa391f50185357e99f2a7808e293638a34f4d729defc7eba669b66c0da8fcd"
+         ),
+         .binaryTarget(
+             name: "Realm",
+             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.20.7/Realm.xcframework.zip",
+             checksum: "576831b9099d51f3f1f74fb32846e98aebb56bdc473bee29a54feaf6a51ade9a"
+         ),
+         .binaryTarget(
+             name: "RealmSwift",
+             url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.20.7/RealmSwift.xcframework.zip",
+             checksum: "0452dea625e0d6b3e674ae28cff5ab7fcf4442bfd5cb796190fd05b4f42ecb5e"
+         ),
+         .target(
+            name: "AmityUIKitLiveStream"
+         ),
+         .target(
+            name: "UpstraUIKit"
+         )
+     ]
 )
